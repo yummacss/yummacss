a633665b176b9a01dde5aab52175505ba6cf57fe
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-22 19:02:23 +0000

    ref: update config capability from 'core' to 'reset'

--- yummacss.config.js
+++ yummacss.config.js
@@ -2,7 +2,7 @@ export const config = {
   content: [], // Specify paths for CSS scanning.
   output: "", // Path for saving CSS file.
   capabilities: {
-    core: true, // Should I use base styles? — Optional (Default is true)
+    reset: true, // Should I use base styles? — Optional (Default is true)
     minify: true, // Should I minify? — Optional (Default is true)
   },
 };
