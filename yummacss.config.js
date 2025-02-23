dd71e58a7304f798c0687d8bbc31b75d6564f418
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-23 01:31:28 +0000

    ref: Remove yummacss configuration file

--- yummacss.config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  content: [], // Specify paths for CSS scanning.
-  output: "", // Path for saving CSS file.
-  capabilities: {
-    reset: true, // Should I use base styles? — Optional (Default is true)
-    minify: true, // Should I minify? — Optional (Default is true)
-  },
-};
