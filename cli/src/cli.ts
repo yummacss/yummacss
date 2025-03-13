1fcb8aeee3aaf47679472814aeafb94f97328292
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-03-13 20:04:34 +0000

    ref: remove watch command and update version to 3.0.0

--- cli/src/cli.ts
+++ cli/src/cli.ts
@@ -1,14 +1,13 @@
 import { Command } from "commander";
 import { init } from "../commands/init.js";
 import { build } from "../commands/build.js";
-// import { watch } from "../commands/watch.js";
 
 const program = new Command();
 
 program
   .name("yummacss")
   .description("Compiling SCSS and purges styles")
-  .version("0.0.1");
+  .version("3.0.0");
 
 program
   .command("init")
@@ -19,10 +18,5 @@ program
   .command("build")
   .description("Compile SCSS to CSS based on yumma.config.js")
   .action(build);
-
-// program
-//   .command("watch")
-//   .description("Looks for file changes and rebuild as necessary")
-//   .action(watch);
 
 program.parse(process.argv);
