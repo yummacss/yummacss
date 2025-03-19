fdf39442e79ece9e3ced63d952d6011eb4bb0a97
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-03-19 18:37:50 +0000

    feat: add box model utilities for dimensions, margins, and paddings

--- src/core/utilities/box-model.ts
+++ src/core/utilities/box-model.ts
@@ -1,5 +1,387 @@
 import { UtilityMap } from "@/interfaces";
+import { createValues } from "@/core/helpers/create-values";
+import { YMA_DIMENSION, YMA_HEIGHT, YMA_MARGIN, YMA_PADDING, YMA_WIDTH } from "@/constants/variables";
 
 export const boxModelUtils: UtilityMap = {
-    
+  "box-sizing": {
+    prefix: "bs",
+    properties: ["box-sizing"],
+    slug: "box-sizing",
+    values: {
+      bb: "border-box",
+      cb: "content-box",
+    },
+  },
+
+  dimension: {
+    prefix: "d",
+    properties: ["height", "width"],
+    slug: "dimension",
+    values: createValues({
+      base: YMA_DIMENSION,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "max-dimension": {
+    prefix: "max-d",
+    properties: ["max-height", "max-width"],
+    slug: "dimension/#max-dimension",
+    values: createValues({
+      base: YMA_DIMENSION,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "min-dimension": {
+    prefix: "min-d",
+    properties: ["min-height", "min-width"],
+    slug: "dimension/#min-dimension",
+    values: createValues({
+      base: YMA_DIMENSION,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  height: {
+    prefix: "h",
+    properties: ["height"],
+    slug: "height",
+    values: createValues({
+      base: YMA_HEIGHT,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "max-height": {
+    prefix: "max-h",
+    properties: ["max-height"],
+    slug: "height/#max-height",
+    values: createValues({
+      base: YMA_HEIGHT,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "min-height": {
+    prefix: "min-h",
+    properties: ["min-height"],
+    slug: "height/#min-height",
+    values: createValues({
+      base: YMA_HEIGHT,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  margin: {
+    prefix: "m",
+    properties: ["margin"],
+    slug: "margin",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-block-end": {
+    prefix: "mbe",
+    properties: ["margin-block-end"],
+    slug: "margin/#block-end",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-block-start": {
+    prefix: "mbs",
+    properties: ["margin-block-start"],
+    slug: "margin/#block-start",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-bottom": {
+    prefix: "mb",
+    properties: ["margin-bottom"],
+    slug: "margin/#bottom",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-inline-end": {
+    prefix: "mie",
+    properties: ["margin-inline-end"],
+    slug: "margin/#inline-end",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-inline-start": {
+    prefix: "mis",
+    properties: ["margin-inline-start"],
+    slug: "margin/#inline-start",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-left": {
+    prefix: "ml",
+    properties: ["margin-left"],
+    slug: "margin/#left",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-right": {
+    prefix: "mr",
+    properties: ["margin-right"],
+    slug: "margin/#right",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-top": {
+    prefix: "mt",
+    properties: ["margin-top"],
+    slug: "margin/#top",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-x": {
+    prefix: "mx",
+    properties: ["margin-left", "margin-right"],
+    slug: "margin/#x-axis",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "margin-y": {
+    prefix: "my",
+    properties: ["margin-top", "margin-bottom"],
+    slug: "margin/#y-axis",
+    values: createValues({
+      base: YMA_MARGIN,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  padding: {
+    prefix: "p",
+    properties: ["padding"],
+    slug: "padding",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-block-end": {
+    prefix: "pbe",
+    properties: ["padding-block-end"],
+    slug: "padding/#block-end",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-block-start": {
+    prefix: "pbs",
+    properties: ["padding-block-start"],
+    slug: "padding/#block-start",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-bottom": {
+    prefix: "pb",
+    properties: ["padding-bottom"],
+    slug: "padding/#bottom",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-inline-end": {
+    prefix: "pie",
+    properties: ["padding-inline-end"],
+    slug: "padding/#inline-end",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-inline-start": {
+    prefix: "pis",
+    properties: ["padding-inline-start"],
+    slug: "padding/#inline-start",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-left": {
+    prefix: "pl",
+    properties: ["padding-left"],
+    slug: "padding/#left",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-right": {
+    prefix: "pr",
+    properties: ["padding-right"],
+    slug: "padding/#right",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-top": {
+    prefix: "pt",
+    properties: ["padding-top"],
+    slug: "padding/#top",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-x": {
+    prefix: "px",
+    properties: ["padding-left", "padding-right"],
+    slug: "padding/#x-axis",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "padding-y": {
+    prefix: "py",
+    properties: ["padding-top", "padding-bottom"],
+    slug: "padding/#y-axis",
+    values: createValues({
+      base: YMA_PADDING,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  width: {
+    prefix: "w",
+    properties: ["width"],
+    slug: "width",
+    values: createValues({
+      base: YMA_WIDTH,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "max-width": {
+    prefix: "max-w",
+    properties: ["max-width"],
+    slug: "width/#max-width",
+    values: createValues({
+      base: YMA_WIDTH,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
+
+  "min-width": {
+    prefix: "min-w",
+    properties: ["min-width"],
+    slug: "width/#min-width",
+    values: createValues({
+      base: YMA_WIDTH,
+      unit: "rem",
+      min: 0,
+      max: 100,
+    }),
+  },
 };
--- src/core/utilities/index.ts
+++ src/core/utilities/index.ts
@@ -1,24 +1,31 @@
 import { UtilityMap } from "@/interfaces";
+
 import { backgroundUtils } from "./background";
 import { borderUtils } from "./border";
+import { boxModelUtils } from "./box-model";
 import { colorUtils } from "./color";
+import { effectUtils } from "./effect";
 import { flexboxUtils } from "./flexbox";
 import { gridUtils } from "./grid";
 import { outlineUtils } from "./outline";
 import { positioningUtils } from "./positioning";
 import { svgUtils } from "./svg";
+import { tableUtils } from "./table";
 import { transformUtils } from "./transform";
 import { typographyUtils } from "./typography";
 
 export const allUtilities: UtilityMap = {
   ...backgroundUtils,
   ...borderUtils,
+  ...boxModelUtils,
   ...colorUtils,
+  ...effectUtils,
   ...flexboxUtils,
   ...gridUtils,
   ...outlineUtils,
   ...positioningUtils,
   ...svgUtils,
+  ...tableUtils,
   ...transformUtils,
   ...typographyUtils,
 };
