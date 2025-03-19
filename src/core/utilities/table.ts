a4ff4c7e96cadec984b0f614c6e5c7e280778b8a
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-03-19 21:48:38 +0000

    feat: add table utilities

--- src/core/utilities/table.ts
+++ src/core/utilities/table.ts
@@ -1,3 +1,23 @@
 import { UtilityMap } from "@/interfaces";
 
-export const tableUtils: UtilityMap = {};
+export const tableUtils: UtilityMap = {
+  "caption-side": {
+    prefix: "cs",
+    properties: ["caption-side"],
+    slug: "caption-side",
+    values: {
+      t: "top",
+      b: "bottom",
+    },
+  },
+
+  "table-layout": {
+    prefix: "tl",
+    properties: ["table-layout"],
+    slug: "table-layout",
+    values: {
+      a: "auto",
+      f: "fixed",
+    },
+  },
+};
