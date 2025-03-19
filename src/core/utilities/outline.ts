1eb748560b32c92ad5bf4fa790ab8cc25ebfc901
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-03-19 20:55:24 +0000

    feat: add outline utilities

--- src/core/utilities/outline.ts
+++ src/core/utilities/outline.ts
@@ -1,3 +1,40 @@
 import { UtilityMap } from "@/interfaces";
+import { createValues } from "@/core/helpers/create-values";
+import { YMA_OUTLINE_OFFSET, YMA_OUTLINE_WIDTH } from "@/constants/variables";
 
-export const outlineUtils: UtilityMap = {};
+export const outlineUtils: UtilityMap = {
+  "outline-offset": {
+    prefix: "oo",
+    properties: ["outline-offset"],
+    slug: "outline-offset",
+    values: createValues({
+      base: YMA_OUTLINE_OFFSET,
+      unit: "px",
+      min: 0,
+      max: 4,
+    }),
+  },
+
+  "outline-style": {
+    prefix: "os",
+    properties: ["outline-style"],
+    slug: "outline-style",
+    values: {
+      none: "none",
+      d: "dashed",
+      s: "solid",
+    },
+  },
+
+  "outline-width": {
+    prefix: "ow",
+    properties: ["outline-width"],
+    slug: "outline-width",
+    values: createValues({
+      base: YMA_OUTLINE_WIDTH,
+      unit: "px",
+      min: 0,
+      max: 4,
+    }),
+  },
+};
