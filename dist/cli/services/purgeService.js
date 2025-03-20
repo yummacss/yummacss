8157fbc5b16b5be986691e040a9eb09061fd1bfc
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-03-20 23:54:09 +0000

    feat: add default extractor to purgeCSS

--- dist/cli/services/purgeService.js
+++ dist/cli/services/purgeService.js
@@ -3,6 +3,9 @@ export async function purgeCSS(css, config) {
     const purgeCSSResult = await new PurgeCSS().purge({
         content: config.source,
         css: [{ raw: css }],
+        defaultExtractor: (content) => {
+            return content.match(/[\w-/\\:]+/g) || [];
+        },
     });
     return purgeCSSResult[0].css;
 }
