6bea66e320b8cf7a47eaee9864b57bb85e1ec347
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-06-15 17:27:46 +0100

    chore: update rate limiting configuration and improve performance in ratelimiter

--- src/lib/ratelimiter.ts
+++ src/lib/ratelimiter.ts
@@ -1,18 +1,24 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 import { getClientIp } from "@/utils/ip";
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export const redis = Redis.fromEnv();
+const cache = new Map();
 
 export const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(10, "10 s"), // 10 requests every 10 seconds
+  limiter: Ratelimit.slidingWindow(20, "10 s"),
   analytics: true,
-  timeout: 10000,
+  timeout: 3000,
+  ephemeralCache: cache,
 });
 
 export async function applyRateLimit(req: NextApiRequest, res: NextApiResponse): Promise<boolean> {
+  if (req.headers.origin === "https://yummacss.com") {
+    return false;
+  }
+
   const identifier = getClientIp(req);
   const result = await ratelimit.limit(identifier);
 
