82d35e746781b97cc08ace994b8d57b97f8aec40
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-07-27 18:47:35 +0100

    style: format CLI files using prettier

--- cli/services/purgeService.ts
+++ cli/services/purgeService.ts
@@ -2,7 +2,10 @@ import { globby } from "globby";
 import { PurgeCSS } from "purgecss";
 import { Configuration } from "../config/defaultConfig.js";
 
-export async function purgeCSS(css: string, config: Configuration): Promise<string> {
+export async function purgeCSS(
+  css: string,
+  config: Configuration
+): Promise<string> {
   const purgeCSSResult = await new PurgeCSS().purge({
     content: await globby(config.source),
     css: [{ raw: css }],
