4589a61240288715a26cc8a6c9eabc76648f8f2d
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-09-10 17:15:24 +0100

    🧹

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["cli/src/index.ts"],
-  unbundle: true,
   clean: true,
+  entry: ["cli/src/index.ts"],
   format: ["esm"],
   minify: true,
   outDir: "dist",
   sourcemap: false,
   target: "es2020",
+  unbundle: true,
 });
 
