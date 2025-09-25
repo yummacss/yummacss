8889f61da874228ecebcdf1118e6db3369c254c9
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-09-25 19:02:17 +0100

    feat: add shebang to index.js and update tsdown configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  banner: { js: "#!/usr/bin/env node" },
   entry: ["cli/src/index.ts"],
   format: ["esm"],
   minify: true,
