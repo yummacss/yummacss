48766b3c51d15fdf7ce05e4d49ac4091330eac6c
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-10-10 15:20:18 +0100

    chore(cli): update description and add version

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -2,10 +2,11 @@ import { Command } from "commander";
 import { build } from "./commands/build.js";
 import { init } from "./commands/init.js";
 import { watch } from "./commands/watch.js";
+import { version } from "../../package.json";
 
 const cli = new Command();
 
-cli.name("yummacss").description("Main command");
+cli.name("yummacss").description("Yumma CSS main command").version(version);
 
 cli.command("init").action(init).alias("i").description("Initialize config");
 
