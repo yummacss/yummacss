308304aae855325eddcafdff7e844abd49a8a633
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-11-11 23:49:19 +0000

    feat: add browser entry point and exports for browser-safe modules

---
+++ src/browser.ts
@@ -0,0 +1,4 @@
+// browser-safe exports
+export { generator } from "./generator";
+export { baseCSS } from "./reset/base";
+export { tokenizer } from "./tokenizer";
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+	entry: ["src/index.ts", "src/browser.ts"],
 	clean: true,
 	dts: true,
 	format: ["esm"],
