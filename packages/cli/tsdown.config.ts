2b0923b8b1d8d469a5f7fac3654a0001a793bf29
Renildo Pereira <rrenildopereiraa@gmail.com>
2026-01-15 21:54:26 +0000

    chore: convert to monorepo structure

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
 	banner: { js: "#!/usr/bin/env node" },
-	entry: ["cli/src/index.ts"],
+	entry: ["src/index.ts"],
 	format: ["esm"],
 	minify: true,
 	target: "es2020",
